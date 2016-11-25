de18500346a193bc335915f470dc338ae55d3c3a
Hideo Hattori <hattori-h@klab.com>
2016-11-25 11:38:29 +0900

    add filter option

--- findbigdiff.py
+++ findbigdiff.py
@@ -15,6 +15,7 @@ def _main(lines, opts):
     stacks = []
     one_stackinfo = []
     percent = 0.0
+    filename = ""
 
     # parse
     for line in lines:
@@ -26,11 +27,13 @@ def _main(lines, opts):
                 is_first = False
             else:
                 stacks.append({
+                    "filename": filename,
                     "percent": percent,
                     "info": "".join([stack for stack in one_stackinfo])
                 })
                 one_stackinfo = []
                 percent = 0.0
+            filename = line.split()[-1]
         elif RE_TOTAL_DIFF_PERCENT_LINE.search(line):
             s = RE_TOTAL_DIFF_PERCENT_LINE.search(line)
             percent = float(s.group(1))
@@ -38,6 +41,7 @@ def _main(lines, opts):
 
     # care for last info
     stacks.append({
+        "filename": filename,
         "percent": percent,
         "info": "".join([stack for stack in one_stackinfo])
     })
@@ -45,6 +49,8 @@ def _main(lines, opts):
     # sort and output
     sorted_stacks = sorted(stacks, key=lambda i: i["percent"], reverse=not opts.reverse)
     for stackinfo in sorted_stacks:
+        if opts.filter and not opts.filter.search(stackinfo["filename"]):
+            continue
         if stackinfo["percent"] < opts.threshold:
             continue
         print("%s" % stackinfo["info"])
@@ -60,6 +66,7 @@ def main():
     parser.add_argument('-t', '--threshold', type=float, default=THRESHOLD,
                         help='difference threshold. (default: {0:.1f}[%%])'.format(THRESHOLD))
     parser.add_argument('-r', '--reverse', action='store_true', help='sort reverse by difference percent')
+    parser.add_argument('--filter', type=re.compile, help='print only regex match filename')
     parser.add_argument('file', nargs='?', type=argparse.FileType('r+'),
                         help="pprof's difference list text")
 
